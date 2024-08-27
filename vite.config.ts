35a51c1416a26c9a629970971c28e7b3673b2839
vladimir.chikaidze <>
2024-08-27 21:19:03 +0400

    Исправил ошибку из-за которой vite не мог стартануть

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // plugins: [react()],
+  plugins: [react()],
   // server: {
   //   proxy: {
   //     '/api': {
